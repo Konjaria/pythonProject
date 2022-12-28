11a057eb0646b1449d79b3a108dc21c65a165a14
Konjaria <konjaria1010@gmail.com>
2022-12-29 03:57:11 +0400

    Hey Please commit this one as well

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 from classes.ui_setup import main_window
-import pandas as pd
 
 
 def main():
-    df = pd.read_csv(filepath_or_buffer="data_input/NYPD_Shooting_Incident_Data__Historic_.csv")
-    my_ui = main_window(database=df)
+    my_ui = main_window()
 
 
 if __name__ == "__main__":
